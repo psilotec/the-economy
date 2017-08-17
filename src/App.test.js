59a298835d67da1e6c8f58e7b7ab7b66085ff2b0
Scott DeVito <scottadevito@gmail.com>
2017-08-17 14:59:11 -0400

    Add tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import Header from './components/header';
+import EconomyMainContainer from './containers/economy_main_container';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('renders the Header component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(Header)).toHaveLength(1);
+});
+
+it('renders the EconomyMainContainer component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(EconomyMainContainer)).toHaveLength(1);
 });
